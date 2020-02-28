d4c0755385ba686386273a6c1e0b332c451da72c
Watayo <ryo0616mani@gmail.com>
2020-02-28 15:05:49 +0000

    heroku line-login

--- app.rb
+++ app.rb
@@ -25,3 +25,20 @@ end
 get '/login' do
   erb :login
 end
+
+get '/callback' do
+  uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
+  request = Net::HTTP::Post.new(uri)
+  request.content_type = "application/x-www-form-urlencoded"
+  request.set_form_data(
+    "client_id" => @line_key,
+    "client_secret" => @line_secret,
+    "code" => params[:code],
+    "grant_type" => "authorization_code",
+    "redirect_uri" => "https://boiling-garden-62154.herokuapp.com/callback",
+  )
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+end
